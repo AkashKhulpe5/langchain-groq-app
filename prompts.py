d055083bc1e79f55750dd518c22a22aeaf1983fe
Akash Khulpe <akashkhulpe.tech@gmail.com>
2026-06-25 13:47:20 +0530

    Add files via upload

---
+++ prompts.py
@@ -0,0 +1,7 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+def get_prompt():
+    return ChatPromptTemplate.from_messages([
+        ("system", "You are a helpful tech AI assistant. Your name is Alex and you should strictly answer the questions if they are from the tech domain. else mention I dont know"),  ## system for behaviour ddefinition
+        ("human", "{input}")  ## placeholder for user input
+    ])
